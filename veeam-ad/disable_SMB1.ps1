414b981a57aef7a0ef37846ed9c1e5e105c88594
csternig <christian@sternig.net>
2018-01-01 22:25:46 +0100

    +gitognore File +Disable SMB1 PowerShell +setupAD PowerShell Skript
    TODO: integrate in Packer or vagrant
    
    Signed-off-by: csternig <christian@sternig.net>

---
+++ veeam-ad/disable_SMB1.ps1
@@ -0,0 +1,16 @@
+#Should run before ./scripts/win-updates.ps1
+
+if (!(New-Object System.Security.Principal.WindowsPrincipal(
+    [Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    throw 'this must run with Administrator privileges (e.g. in a elevated shell session)'
+}
+
+Write-Output "Testing if SMB1 is active and installed..."
+if (Get-WindowsFeature FS-SMB1) {
+    Write-Output "SMB1 is active, removing the feature"
+    Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
+    }
+	
+	
+	
---
+++ veeam-ad/setup_ActiveDirectory.ps1
@@ -0,0 +1,7 @@
+if (!(New-Object System.Security.Principal.WindowsPrincipal(
+    [Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    throw 'this must run with Administrator privileges (e.g. in a elevated shell session)'
+}
+
+
